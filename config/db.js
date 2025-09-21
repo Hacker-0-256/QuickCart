f91e57d2cd291c95f43e6fd834ed1941d00b2a6b
Hacker-0-256 <studentalexshyaka@gmail.com>
2025-09-21 19:29:44 +0200

    First commit

---
+++ config/db.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+let cache = global.mongoose;
+if (!cache) {
+  cache = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+    if (cache.conn) {
+        return cache.conn;
+    }
+
+    if (!cache.promise) {
+        const opts = {
+            bufferCommands: false,
+        }
+        cache.promise = mongoose.connect('${process.env.MONGODB_URI}/quickcart', opts).then((mongoose) => {
+            return mongoose;
+        })
+
+    }
+
+    cache.conn = await cache.promise;
+    return cache.conn;
+}   
+
+
+export default dbConnect
---
+++ models/user.js
@@ -0,0 +1,14 @@
+import { User } from "@clerk/nextjs/dist/types/server";
+import mongoose from "mongoose";   
+
+const userSchema = new mongoose.Schema({
+    id:{type: String, required: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    imageurl: {type: String, required: true},
+    cartItems: {type: Object, default: {}},
+},    {minimize: false})
+
+const Userext = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
